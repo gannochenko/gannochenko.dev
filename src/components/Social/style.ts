085a535995336039a4ca4842abaf855df80fafd5
Sergei <gannochenko.sv@gmail.com>
2019-10-27 19:50:18 +0100

    ui port in progress

--- src/components/Social/style.ts
+++ src/components/Social/style.ts
@@ -2,6 +2,6 @@ import styled from 'styled-components';
 import { rectangle } from '@bucket-of-bolts/styled-companion';
 
 export const Image = styled.img`
-    ${rectangle('2rem')}
+    ${rectangle('2rem')};
     user-select: none;
 `;
