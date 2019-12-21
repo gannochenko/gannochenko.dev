576807ed3c38f94b62a8477a14f283f8e7e1a0da
Sergei <gannochenko.sv@gmail.com>
2019-12-21 16:48:02 +0100

    idea

--- src/lib/effects.tsx
+++ src/lib/effects.tsx
@@ -36,46 +36,99 @@ const IDGenerator = function*() {
 
 export const idGenerator = IDGenerator();
 
-const Effect: FunctionComponent<{ children: any; effectTimeout?: number }> = ({
-    children,
-    effectTimeout = 0,
-}) => {
-    const nodeId = useMemo(() => {
-        return idGenerator.next().value;
-    }, []);
-    const [runEffect, setRunEffect] = useState(false);
+interface EffectPropss {
+    children: any;
+    effectTimeout?: number;
+}
 
-    const onEventFire = (id: string) => {
-        if (id.toString() === nodeId.toString()) {
-            setTimeout(() => setRunEffect(true), effectTimeout);
-            eventEmitter.off(EVENT_EFFECT_RUN, onEventFire);
+interface EffectState {
+    nodeId: string;
+    runEffect: boolean;
+}
+
+class Effect extends React.Component<EffectPropss, EffectState> {
+    constructor(props: EffectPropss) {
+        super(props);
+        this.state = {
+            nodeId: idGenerator.next().value,
+            runEffect: false,
+        };
+    }
+
+    componentDidMount() {
+        eventEmitter.on(EVENT_EFFECT_RUN, this.onEventFire);
+        console.log('ready = ' + this.state.nodeId);
+        setTimeout(
+            () => eventEmitter.emit(EVENT_ELEMENT_READY, [this.state.nodeId]),
+            100,
+        );
+    }
+
+    componentWillUnmount() {
+        eventEmitter.off(EVENT_EFFECT_RUN, this.onEventFire);
+    }
+
+    onEventFire = (id: string) => {
+        const { nodeId } = this.state;
+        const { effectTimeout } = this.props;
+
+        if (id.toString() === nodeId) {
+            setTimeout(() => this.setState({ runEffect: true }), effectTimeout);
+            eventEmitter.off(EVENT_EFFECT_RUN, this.onEventFire);
         }
     };
 
-    useEffect(() => {
-        eventEmitter.on(EVENT_EFFECT_RUN, onEventFire);
-        return () => {
-            eventEmitter.off(EVENT_EFFECT_RUN, onEventFire);
-        };
-    }, [eventEmitter, onEventFire]);
+    render() {
+        const { nodeId, runEffect } = this.state;
 
-    const effectProps = useMemo(
-        () => ({
+        return this.props.children({
             [EFFECT_DATA_ATTRIBUTE]: nodeId,
             className: 'effects-node',
             runEffect,
-        }),
-        [runEffect, nodeId],
-    );
+        });
+    }
+}
 
-    const html = children(effectProps);
-
-    useEffect(() => {
-        setTimeout(() => eventEmitter.emit(EVENT_ELEMENT_READY, [nodeId]), 100);
-    }, []);
-
-    return html;
-};
+// const Effect: FunctionComponent<EffectPropss> = ({
+//     children,
+//     effectTimeout = 0,
+// }) => {
+//     const nodeId = useMemo(() => {
+//         return idGenerator.next().value;
+//     }, []);
+//     const [runEffect, setRunEffect] = useState(false);
+//
+//     const onEventFire = (id: string) => {
+//         if (id.toString() === nodeId.toString()) {
+//             setTimeout(() => setRunEffect(true), effectTimeout);
+//             eventEmitter.off(EVENT_EFFECT_RUN, onEventFire);
+//         }
+//     };
+//
+//     useEffect(() => {
+//         eventEmitter.on(EVENT_EFFECT_RUN, onEventFire);
+//         return () => {
+//             eventEmitter.off(EVENT_EFFECT_RUN, onEventFire);
+//         };
+//     }, [eventEmitter, onEventFire]);
+//
+//     const effectProps = useMemo(
+//         () => ({
+//             [EFFECT_DATA_ATTRIBUTE]: nodeId,
+//             className: 'effects-node',
+//             runEffect,
+//         }),
+//         [runEffect, nodeId],
+//     );
+//
+//     const html = children(effectProps);
+//
+//     useEffect(() => {
+//         setTimeout(() => eventEmitter.emit(EVENT_ELEMENT_READY, [nodeId]), 100);
+//     }, []);
+//
+//     return html;
+// };
 
 export const withEffects = (Component: any) => {
     const WithEffects = (props: ObjectLiteral) => {
