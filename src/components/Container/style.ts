1d64b040291171271403aa89d11edcf977d7c211
Sergei <gannochenko.sv@gmail.com>
2019-12-26 23:54:26 +0200

    container fix

--- src/components/Container/style.ts
+++ src/components/Container/style.ts
@@ -9,7 +9,7 @@ export const ContainerStandard = styled.div`
     margin: 0 auto;
     width: auto;
     max-width: 60%;
-    ${media({ xs: 'max-size: 90%', sm: 'max-size: 70%' })};
+    ${media({ xs: 'max-width: 90%', sm: 'max-width: 70%' })};
     padding: 0 1rem;
 `;
 
@@ -17,6 +17,6 @@ export const ContainerNarrow = styled.div`
     margin: 0 auto;
     width: auto;
     max-width: 40%;
-    ${media({ xs: 'max-size: 90%', sm: 'max-size: 60%' })};
+    ${media({ xs: 'max-width: 90%', sm: 'max-width: 60%' })};
     padding: 0 1rem;
 `;
