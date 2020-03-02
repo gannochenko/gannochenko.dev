88bff8d34491acbd4127ffe21d447994b30b93ba
Sergei <gannochenko.sv@gmail.com>
2020-03-02 20:48:58 +0100

    unused stuff removed

--- src/components/Avatar/Avatar.tsx
+++ src/components/Avatar/Avatar.tsx
@@ -4,20 +4,16 @@ import { Query } from './query';
 
 import { Props } from './props';
 
-export const Avatar: FunctionComponent<Props> = props => {
-    const { size = '12rem', borderSize = '0.2rem' } = props;
-
-    console.log(props.title);
-
+export const Avatar: FunctionComponent<Props> = ({
+    size = '12rem',
+    borderSize = '0.2rem',
+}) => {
     return (
         <Query>
             {data => (
-                <>
-                    <Container size={size} borderSize={borderSize}>
-                        <Image sizes={data.avatar.childImageSharp.fluid} />
-                    </Container>
-                    T: {props.title}
-                </>
+                <Container size={size} borderSize={borderSize}>
+                    <Image sizes={data.avatar.childImageSharp.fluid} />
+                </Container>
             )}
         </Query>
     );
