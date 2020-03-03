bc9ebee1e2b4303a5ad941533c4c259910b279a9
Sergei <gannochenko.sv@gmail.com>
2020-03-03 23:54:04 +0100

    legacy code removed

--- src/widgets/Skills/components/Hexagon/style.ts
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-import { Props } from './type';
-
-export const Shape = styled.path<Props>`
-    fill: ${props => (props.filled ? '#9e3333' : 'none')};
-    stroke: #9e3333;
-    stroke-miterlimit: 10;
-    stroke-width: 0.3rem;
-`;
--- src/widgets/Skills/components/Skill/components/TextFit/TextFit.tsx
+++
@@ -1,8 +0,0 @@
-import React, { FunctionComponent } from 'react';
-
-import { TextFitContainer } from './style';
-import { Props } from './type';
-
-export const TextFit: FunctionComponent<Props> = ({ children, size }) => {
-    return <TextFitContainer size={size}>{children}</TextFitContainer>;
-};
