8d755fb440ffee69cf73b687eeafba0370fb7165
Sergei <gannochenko.sv@gmail.com>
2020-03-04 22:01:31 +0100

    wip

--- gatsby-config.js
+++ gatsby-config.js
@@ -87,6 +87,7 @@ module.exports = {
             },
         },
         'gatsby-plugin-styled-components',
+        'gatsby-plugin-emotion',
         'gatsby-plugin-typescript',
         'gatsby-plugin-catch-links',
     ],
--- src/components/Header/components/HeaderMain/style.ts
+++ src/components/Header/components/HeaderMain/style.ts
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled from '@emotion/styled';
 import Img from 'gatsby-image';
 import {
     absoluteCover,
