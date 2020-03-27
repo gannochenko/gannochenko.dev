3c4795b7c78f5c51f01ab59190d18b347305fc63
Sergei <gannochenko.sv@gmail.com>
2020-03-27 22:39:58 +0100

    anchor

--- src/components/Typography/Typography.tsx
+++ src/components/Typography/Typography.tsx
@@ -1,20 +1,52 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, useMemo } from 'react';
 
 import { Props } from './type';
-import { H1, H2, H3 } from './style';
+import { H1, H2, H3, Anchor } from './style';
 
 export const Typography: FunctionComponent<Props> = ({
     sub,
     subSub,
+    children,
     ...restProps
 }) => {
+    const showAnchor = subSub || sub;
+    const aKey = useMemo(() => {
+        if (!showAnchor) {
+            return '';
+        }
+
+        return children!
+            .toString()
+            .toLowerCase()
+            .replace(/\s+/g, '_')
+            .replace(/[^a-z0-9_-]/g, '');
+    }, [showAnchor, children]);
+
     if (sub) {
-        return <H2 {...restProps} />;
+        return (
+            <H2 {...restProps}>
+                {children}{' '}
+                {showAnchor && (
+                    <Anchor href={`#${aKey}`} name={aKey}>
+                        #
+                    </Anchor>
+                )}
+            </H2>
+        );
     }
 
-    if (subSub) {
-        return <H3 {...restProps} />;
+    if (sub) {
+        return (
+            <H3 {...restProps}>
+                {children}{' '}
+                {showAnchor && (
+                    <Anchor href={`#${aKey}`} name={aKey}>
+                        #
+                    </Anchor>
+                )}
+            </H3>
+        );
     }
 
-    return <H1 {...restProps} />;
+    return <H1 {...restProps}>{children}</H1>;
 };
--- src/components/Typography/style.ts
+++ src/components/Typography/style.ts
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { fgColor } from '@bucket-of-bolts/styled-companion/build';
 
 export const H1 = styled.h1`
     margin: 4rem 0 2rem 0;
@@ -13,4 +14,14 @@ export const H2 = styled.h2`
 export const H3 = styled.h3`
     margin: 2rem 0 2rem 0;
     font-size: 1.3rem;
+`;
+
+export const Anchor = styled.a`
+    text-decoration: none;
+    ${props =>
+        fgColor(
+            props.theme.palette.siskinSproutYellow,
+            props.theme.color.link.normal,
+            '300ms',
+        )}
 `;
