fddee906a87db841ecd09ba0769c051a58612b9c
Sergei <gannochenko.sv@gmail.com>
2020-03-29 14:44:21 +0200

    ga

--- gatsby-config.js
+++ gatsby-config.js
@@ -89,5 +89,32 @@ module.exports = {
         'gatsby-plugin-styled-components',
         'gatsby-plugin-typescript',
         'gatsby-plugin-catch-links',
+        {
+            resolve: `gatsby-plugin-google-analytics`,
+            options: {
+                // The property ID; the tracking code won't be generated without it
+                trackingId: 'UA-162152239-1',
+                // Defines where to place the tracking script - `true` in the head and `false` in the body
+                head: false,
+                // Setting this parameter is optional
+                anonymize: true,
+                // Setting this parameter is also optional
+                respectDNT: true,
+                // Avoids sending pageview hits from custom paths
+                // exclude: ["/preview/**", "/do-not-track/me/too/"],
+                // Delays sending pageview hits on route update (in milliseconds)
+                pageTransitionDelay: 0,
+                // Enables Google Optimize using your container Id
+                // optimizeId: "YOUR_GOOGLE_OPTIMIZE_TRACKING_ID",
+                // Enables Google Optimize Experiment ID
+                // experimentId: "YOUR_GOOGLE_EXPERIMENT_ID",
+                // Set Variation ID. 0 for original 1,2,3....
+                // variationId: "YOUR_GOOGLE_OPTIMIZE_VARIATION_ID",
+                // Any additional optional fields
+                // sampleRate: 5,
+                // siteSpeedSampleRate: 10,
+                // cookieDomain: "example.com",
+            },
+        },
     ],
 };
