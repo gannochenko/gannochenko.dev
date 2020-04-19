25fa77b7b35c6fd3a27e5e26a3240365051040ab
Sergei <gannochenko.sv@gmail.com>
2020-04-19 23:35:31 +0200

    article draft and fixes

--- src/components/Listing/components/CodeContainer/style.ts
+++ src/components/Listing/components/CodeContainer/style.ts
@@ -8,15 +8,20 @@ export const CodeContainerContainer = styled.div<{
         wide ? bgColor : 'transparent'};
     line-height: 1.4rem;
     position: relative;
+    margin: 2rem 0;
     ${({ theme }) =>
         theme.util.media({
             xs: 'font-size: 1.1rem;',
         })}
+
+    .token-line:last-child {
+        display: none;
+    }
 `;
 
 export const CodeKey = styled.div`
     position: absolute;
-    top: 20px;
+    top: 16px;
     left: 0.5rem;
     font-size: 1rem;
     color: ${props => props.color || 'inherit'};
@@ -26,7 +31,7 @@ export const CodeKey = styled.div`
 export const Wrapper = styled.div<{ bgColor: string }>`
     background-color: ${props => props.bgColor};
     margin: 0 -1.5rem;
-    padding: 0.2rem 1.5rem 0 1.5rem;
+    padding: 0 1.5rem;
     border-radius: 5px;
     overflow-x: auto;
 `;
