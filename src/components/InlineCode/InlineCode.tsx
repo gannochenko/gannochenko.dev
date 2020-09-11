b030d91f323c724f1499b365d5ea12eda4fbeac7
Sergei <gannochenko.sv@gmail.com>
2020-09-11 23:32:37 +0200

    wip

--- src/components/InlineCode/InlineCode.tsx
+++ src/components/InlineCode/InlineCode.tsx
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const InlineCode = styled.span`
     padding: 0.2rem 0.5rem;
-    background-color: #edeff1;
+    background-color: #f0eeec;
     border-radius: 3px;
     font-family: monospace, monospace;
 `;
