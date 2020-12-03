c5020bbae097de7a4364c8801abd72a7dd09d27c
Sergei <gannochenko.sv@gmail.com>
2020-12-03 23:12:16 +0100

    wip

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -31,7 +31,7 @@ export const Menu: FunctionComponent<Props> = () => {
                 <Right>
                     <Items>
                         <Item to="/blog">Blog</Item>
-                        {/*<Item to="/projects">Projects</Item>*/}
+                        {/*<Item to="/projects">Pet Projects</Item>*/}
                         <Item to="/techradar">Techradar</Item>
                         {/*<Item to="/story">Story</Item>*/}
                         {/*<Item to="/contact">Contact</Item>*/}
@@ -48,7 +48,7 @@ export const Menu: FunctionComponent<Props> = () => {
                     Blog
                 </MobileItem>
                 {/*<MobileItem to="/projects" onClick={onMobileItemClick}>*/}
-                {/*    Projects*/}
+                {/*    Pet Projects*/}
                 {/*</MobileItem>*/}
                 <MobileItem to="/techradar" onClick={onMobileItemClick}>
                     Techradar
