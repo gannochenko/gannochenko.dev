339a0e56e62edd56876c8a5836daf5f514137040
Sergei <gannochenko.sv@gmail.com>
2020-12-25 14:37:02 +0100

    wip

--- src/components/ProjectDetailsPageLayout/components/ProjectLinks/ProjectLinks.tsx
+++ src/components/ProjectDetailsPageLayout/components/ProjectLinks/ProjectLinks.tsx
@@ -6,9 +6,13 @@ import { backgroundCover, colorLynch, foregroundColor } from '@gannochenko/etc';
 const githubLogo = require('./github.png') as string;
 const poolIcon = require('./pool.svg') as string;
 const dockerIcon = require('./docker.png') as string;
+const npmIcon = require('./npm.svg') as string;
 
 const ProjectLinksRoot = styled(Container)`
     margin-bottom: 5rem;
+`;
+
+export const ProjectLinksLinks = styled.div`
     display: flex;
 
     & > *:not(:last-child) {
@@ -24,51 +28,106 @@ export const ProjectLinksLink = styled.a`
     ${() => foregroundColor(colorLynch, 'inherit', '300ms')}
 `;
 
-const ProjectLinksGithubLogo = styled.div<{ logo: string }>`
+const ProjectLinksLogo = styled.div<{ logo: string }>`
     ${({ logo }) => backgroundCover(logo)};
     height: 2rem;
     width: 2rem;
     margin-right: 0.5rem;
 `;
 
+export const ProjectLinksCredit = styled.div`
+    font-size: 0.7rem;
+    margin-top: 1.5rem;
+
+    color: lightgray;
+    a,
+    a:hover,
+    a:active,
+    a:visited,
+    a:focus {
+        color: lightgray;
+    }
+`;
+
 type ProjectLinksPropsType = {
     source?: string;
     article?: string;
     dockerhub?: string;
+    npm?: string;
 };
 
 export const ProjectLinks: FC<ProjectLinksPropsType> = ({
     source,
     article,
     dockerhub,
+    npm,
 }) => {
     return (
         <ProjectLinksRoot>
-            {!!source && (
-                <ProjectLinksLink
-                    href={source}
-                    target="_blank"
-                    rel="noreferrer nofollower"
-                >
-                    <ProjectLinksGithubLogo logo={githubLogo} />
-                    Source code
-                </ProjectLinksLink>
-            )}
-            {!!article && (
-                <ProjectLinksLink href={article}>
-                    <ProjectLinksGithubLogo logo={poolIcon} />
-                    Howto article
-                </ProjectLinksLink>
-            )}
-            {!!dockerhub && (
-                <ProjectLinksLink
-                    href={dockerhub}
-                    target="_blank"
-                    rel="noreferrer nofollower"
-                >
-                    <ProjectLinksGithubLogo logo={dockerIcon} />
-                    Docker Image
-                </ProjectLinksLink>
+            <ProjectLinksLinks>
+                {!!source && (
+                    <ProjectLinksLink
+                        href={source}
+                        target="_blank"
+                        rel="noreferrer nofollower"
+                    >
+                        <ProjectLinksLogo logo={githubLogo} />
+                        Source code
+                    </ProjectLinksLink>
+                )}
+                {!!article && (
+                    <ProjectLinksLink href={article}>
+                        <ProjectLinksLogo logo={poolIcon} />
+                        Howto article
+                    </ProjectLinksLink>
+                )}
+                {!!dockerhub && (
+                    <ProjectLinksLink
+                        href={dockerhub}
+                        target="_blank"
+                        rel="noreferrer nofollower"
+                    >
+                        <ProjectLinksLogo logo={dockerIcon} />
+                        Docker Image
+                    </ProjectLinksLink>
+                )}
+                {!!npm && (
+                    <ProjectLinksLink
+                        href={npm}
+                        target="_blank"
+                        rel="noreferrer nofollower"
+                    >
+                        <ProjectLinksLogo logo={npmIcon} />
+                        NPM
+                    </ProjectLinksLink>
+                )}
+            </ProjectLinksLinks>
+            {!!npm && (
+                <ProjectLinksCredit>
+                    <a
+                        href="https://iconscout.com/icons/npm"
+                        target="_blank"
+                        rel="noreferrer nofollower"
+                    >
+                        Npm Icon
+                    </a>{' '}
+                    by{' '}
+                    <a
+                        href="https://iconscout.com/contributors/fengquanli"
+                        target="_blank"
+                        rel="noreferrer nofollower"
+                    >
+                        Fengquan Li
+                    </a>{' '}
+                    on{' '}
+                    <a
+                        href="https://iconscout.com"
+                        target="_blank"
+                        rel="noreferrer nofollower"
+                    >
+                        Iconscout
+                    </a>
+                </ProjectLinksCredit>
             )}
         </ProjectLinksRoot>
     );
