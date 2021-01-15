e0772da39f2abe5c459ce409de2fb0d9eccd0e8d
Sergei <gannochenko.sv@gmail.com>
2021-01-15 21:51:06 +0100

    wip

--- src/lib/author.tsx
+++ src/lib/author.tsx
@@ -6,7 +6,7 @@ export const AuthorIntro = () => (
     <>
         Business-oriented fullstack engineer, in ❤️ with Tech.
         <br />
-        JS / JS stack: React, Node, Docker, AWS.
+        React, Node, Docker, AWS.
         <br />
         15+ years in dev.
     </>
